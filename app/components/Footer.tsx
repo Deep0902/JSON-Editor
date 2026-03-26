2197e6e22ffe6d61b44145ec37d7acfcbd83f862
Deep Rakesh <deeptank09@gmail.com>
2026-03-26 11:59:38 +0530

    minor adjustment

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="mt-10 border-t border-gray-200 bg-white px-6 py-4 text-sm text-gray-600">
-      <div className="mx-auto flex md:flex-col items-center justify-between gap-3 md:flex-row md:gap-0 md:px-8">
+      <div className="mx-auto flex items-center justify-between gap-3 md:px-8">
         <p className="text-center text-xs md:text-sm">
           Built with ❤️ Need help or want to connect?
         </p>
